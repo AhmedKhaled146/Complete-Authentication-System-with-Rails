efc1c042e3885ab730ffac13e77be31a2f6d1656
AhmedKhaled146 <a7med74yaso@gmail.com>
2024-08-04 21:53:46 +0300

    learn migration and add some attributes to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_04_151204) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_04_184135) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -21,6 +21,9 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_04_151204) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "jti", null: false
+    t.string "username", null: false
+    t.string "phone_number"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
